8d3c9403fefdf404ab13331ac5f79f12ab1d5d6b
Franklyn Rodriguez <franklynrodriguez.swe@gmail.com>
2023-01-24 11:38:00 -0500

    Complete a few routes and started tests

---
+++ tests_routes.py
@@ -0,0 +1,45 @@
+import pytest
+
+
+@pytest.mark.skip
+#def_test_get_favorites_no_saved_favorites():
+#ACT
+#ASSERT
+#assert response.status_code == 200
+
+
+@pytest.mark.skip
+#def_test_get_favorites_not_found():
+#ACT
+#ASSERT
+#assert response.status_code == 400
+
+@pytest.mark.skip
+#def_test_delete_favorited():
+#ACT
+#ASSERT
+#assert response.status_code == 200
+
+@pytest.mark.skip
+#def_test_delete_favorited_not_found():
+#ACT
+#ASSERT
+#assert response.status_code == 200
+
+@pytest.mark.skip
+#def_test_create_account():
+#ACT
+#ASSERT
+#assert response.status_code == 200
+
+@pytest.mark.skip
+#def_test_create_account_must_have_name():
+
+@pytest.mark.skip
+#def_test_create_account_must_have_email_addresse():
+
+@pytest.mark.skip
+#def_test_delete_account():
+
+@pytest.mark.skip
+#def_test_delete_account_not_found():
